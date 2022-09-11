ea49b567e1dd21a6c4ca3afd4bef1481854c391e
Samantha <109199225+Samantha-Sokolis@users.noreply.github.com>
2022-09-11 21:17:54 +1000

    row boxes added

--- develop/script.js
+++ develop/script.js
@@ -1,3 +1,6 @@
 //add current day at the top of the planner (moment)
 var currentTime = moment().format("MMMM Do YYYY");
 $("#currentDay").text(currentTime);
+
+
+//Try this for text input: https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_text_get
