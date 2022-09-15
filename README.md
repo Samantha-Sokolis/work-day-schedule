83703ef4722fe6245c4bb4a3fc2faf4a067b2540
Samantha <109199225+Samantha-Sokolis@users.noreply.github.com>
2022-09-15 16:10:25 +1000

    functio added on-click js

--- README.md
+++ README.md
@@ -35,6 +35,9 @@ THEN the saved events persist
 
 Found past project: read through as reference: https://github.com/ewirtz3/work-day-scheduler/blob/master/index.html
 
+https://www.w3schools.com/jsref/prop_win_localstorage.asp
+https://www.w3schools.com/tags/tag_time.asp
+
 You are required to submit the following for review:
 
 * The URL of the deployed application:
--- develop/script.js
+++ develop/script.js
@@ -3,14 +3,46 @@ var currentTime = moment().format("MMMM Do YYYY");
 $("#currentDay").text(currentTime);
 
 
-//Try this for text input: https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_text_get
-
-// var scheduleNoteEl = $('input[name="schedule-note"]');
-
-// 1. Schedule appears already colour coded - depending on time of day - past, present or future
-
 // 2. Clicking on the time is supposed to allow the note field to be editable
-
 // 3. When note is entered, Save button pressed- item saved in local storage
+// localStorage.setItem("9AM");
+function seeIfSaving(event){
+    // if(event.target.matches(".saveBtn"))
+    if(event.target.className.includes("saveBtn")){
+       // alert("saving...")
+       var row=event.target.closest(".row")
+       var time=row.querySelector(".hour")
+       var note=row.querySelector(".form-input")
+       console.log(time.innerText.trim())
+       console.log(note.value.trim())
+       localStorage.setItem(time.innerText.trim(), note.value.trim())
+    }
+}
+var page = document.getElementById("wholePage");
+page.addEventListener("click", seeIfSaving)
+
+// TODO: Make this not repeatedly done for the other hours
+var textAt9AM = localStorage.getItem("9AM")
+if(textAt9AM) {
+    document.querySelector(".read-9am").value = textAt9AM
+}
+
+// function below saves form input on-click
+function scheduleNote() {
+    document.getElementById("saveBtn").addEventListener("click", "form-input");
+}
+
 
 // 4. Refresh page to ensure item has remained. 
+
+// 1. Schedule appears already colour coded - depending on time of day - past, present or future
+//var currentHour = moment().format("AH");
+   // if(hours >= 9 && hours <= 17){
+     //   $(".hour").addClass("present");
+   // }
+    //elseif(hours < 9) {
+      //  $(".hour").addClass("past");
+    //}
+    //else{
+      //  $(".hour").addClass("future");
+    //}
